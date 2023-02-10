a3c5101b575e39d57c89705147b1911221d8e603
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2023-02-09 17:18:50 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': 0,
+    'indent': 'ignoredNodes',
+    'overrides': [
+      {
+        'rules': {
+          'comma-spacing': 'off',
+          "no-unused-expressions": "off",
+          'semi': 'off',
+          'comma-dangle': 'off'
+        }
+      }
+    ]
+  },
+};
